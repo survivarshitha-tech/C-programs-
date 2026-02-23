c70afe12199c253c6a0405de74e72ef344d29c15
survivarshitha-tech <survivarshitha@gmail.com>
2026-02-23 10:27:26 +0530

    Add files via upload

---
+++ modified each and every digit .c
@@ -0,0 +1,21 @@
+//write a program to print each and every digit from the given number
+//modified
+#include<stdio.h>
+int main()
+{
+    int count=0;
+    int i=2;
+   for(i=2;i<=10;i++);
+   {
+       printf("bye buddy");
+       {
+       printf("ello buddy");
+       }
+       if(i%2==0)
+       {
+           count++;
+       }
+   }
+   printf("count=%d",count);
+    return 0;
+}
