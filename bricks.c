730dc95cda84f02105b158afa6efd9f993286a93
survivarshitha-tech <survivarshitha@gmail.com>
2026-02-27 13:40:14 +0530

    Add files via upload

---
+++ bricks.c
@@ -0,0 +1,48 @@
+// We want to make a row of bricks that is goal inches long.We have a number of small bricks (1 inch each) and big bricks (5 inches each).Return true if it is possible to make the goal by choosing from the given bricks.
+
+#include <stdio.h>
+
+int main()
+{
+    int small, big, goal;
+
+    printf("Enter the number of small bricks:\n");
+    scanf("%d", &small);
+
+    printf("Enter the number of big bricks:\n");
+    scanf("%d", &big);
+
+    printf("Enter the goal in inches:\n");
+    scanf("%d", &goal);
+
+    int req = goal / 5;
+
+    if (req <= big)
+    {
+        int rem = goal - req * 5;
+
+        if (rem <= small)
+        {
+            printf("true");
+        }
+        else
+        {
+            printf("false");
+        }
+    }
+    else
+    {
+        int rem = goal - big * 5;
+
+        if (rem <= small)
+        {
+            printf("true");
+        }
+        else
+        {
+            printf("false");
+        }
+    }
+
+    return 0;
+}
